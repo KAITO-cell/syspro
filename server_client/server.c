92a7a8f887eafa788cdec4186583cf087a87a953
KAITO <kagiter15@gmail.com>
2022-01-19 17:05:30 +0900

    modify detail

--- server_client/server.c
+++ server_client/server.c
@@ -18,6 +18,7 @@ int main(int argc, char** argv){
 	////struct sockaddr_in server_addr, client_addr;
 	struct sockaddr_in client_addr;
 	socket_data server;
+	socket_data client;
 	int c_sockfd;
 	int pid;
 	socklen_t sin_siz;
@@ -34,10 +35,10 @@ int main(int argc, char** argv){
 	int *status;
 	while(1){
 		fprintf(stdout, "waiting connect...\n");
-		if((c_sockfd = accept(server.sockfd, (struct sockaddr *)&client_addr, &sin_siz))<0){
+		if((client.sockfd = accept(server.sockfd, (struct sockaddr *)&client.addr, &sin_siz))<0){
 			perror("accept\n");
 			close(server.sockfd);
-			close(c_sockfd);
+			close(client.sockfd);
 			exit(EXIT_FAILURE);
 		}
 		pid = fork();
@@ -49,27 +50,27 @@ int main(int argc, char** argv){
 			fprintf(stderr, "child: pid=%d\n",child_pid);
 			//close server socketfd
 			close(server.sockfd);
-			recv_size = recv(c_sockfd, recv_buf, BUF_SIZE, 0);
+			recv_size = recv(client.sockfd, recv_buf, BUF_SIZE, 0);
 			strcpy(name, recv_buf);
-			printf("connect from %s: %d[%s]\n",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port),name);
+			printf("connect from %s: %d[%s]\n",inet_ntoa(client.addr.sin_addr), ntohs(client.addr.sin_port),name);
 			send_size =sprintf(send_buf, "hello [%s]\n",name);
-			send_size = send(c_sockfd, send_buf, send_size, 0);
+			send_size = send(client.sockfd, send_buf, send_size, 0);
 			memset(recv_buf, 0, BUF_SIZE);
 		    memset(send_buf, 0, BUF_SIZE);
 			while(1){
 
 				//fprintf(stderr,"wait for client\n");
-				recv_size = recv(c_sockfd, recv_buf, BUF_SIZE, 0);
+				recv_size = recv(client.sockfd, recv_buf, BUF_SIZE, 0);
 				
 				//printf("from %d[%s]: %s\n",getpid(),name,recv_buf);
-				printf("from [%s]: %s\n",name,recv_buf);
-				check_recive_size(recv_size,c_sockfd);
+				printf("[%s]: %s\n",name,recv_buf);
+				check_recive_size(recv_size,client.sockfd);
 				if(strcmp(recv_buf, "game") == 0) {
-					start_question(c_sockfd);
+					start_question(client.sockfd);
 				}
-				if(strcmp(recv_buf, "exit")==0) break;
+				if(strcmp(recv_buf, "goodbye")==0) break;
 				send_size =sprintf(send_buf, "get [%s] from you\n",recv_buf);
-				send_size = send(c_sockfd, send_buf, send_size, 0);
+				send_size = send(client.sockfd, send_buf, send_size, 0);
 				memset(recv_buf, 0, BUF_SIZE);
 			    memset(send_buf, 0, BUF_SIZE);
 				
@@ -77,15 +78,15 @@ int main(int argc, char** argv){
 			// send_size =sprintf(send_buf, "start question");
         	// send_size =(sockfd,send_buf,send_size, 0);
 			
-			fprintf(stderr, "finish while recv send\n");
-			if(strcmp(recv_buf, "exit")==0){
+			//fprintf(stderr, "finish while communicate\n");
+			if(strcmp(recv_buf, "goodbye")==0){
 				//send exit
 				//send_size = send(c_sockfd, recv_buf, recv_size, 0);
 				//recv exit response
 				//recv_size = recv(c_sockfd, recv_buf, recv_size, 0);
 				//send goodbymessage
 				send_size =sprintf(send_buf, "goodbye");
-				send_size = send(c_sockfd, send_buf, send_size, 0);
+				send_size = send(client.sockfd, send_buf, send_size, 0);
 				printf("%s is logout\n",name);
 				close(c_sockfd);
 				exit(EXIT_SUCCESS);
@@ -93,7 +94,7 @@ int main(int argc, char** argv){
 			//finish child process
 		}else{//parent process
 			fprintf(stderr, "parent: pid=%d\n",pid);
-			close(c_sockfd);
+			close(client.sockfd);
 			//wait(status);
 			//if(WIFEXITED(*status)) {
 			//	printf("Exit: %d\n", WEXITSTATUS(*status));
@@ -101,7 +102,7 @@ int main(int argc, char** argv){
 		}
 	}
 	printf("finish::::\n");
-	close(c_sockfd);
+	close(client.sockfd);
 	close(server.sockfd);
 	return 0;
 }
